58c82f52864c1cb252bcfd313e57fa81e3e0a9f4
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-03-17 23:16:32 +0300

    Init commit

---
+++ IlymaxShop/IlymaxShop/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 17.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    }
+}
+
