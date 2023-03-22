9384460f5b00b20dab2285d7345c7dcc8e387f67
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-03-22 18:49:09 +0300

    Created 2 presenter for Controllers

---
+++ IlymaxShop/IlymaxShop/Presenters/SignUpPresenter.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpPresenter.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 22.03.2023.
+//
+
+import Foundation
+
+
+class SignUpPresenter {
+    
+    var view: SignUpViewController?
+    private var authenticationService: AuthenticationService = MockAuthenticationService.shared
+    private var userService: UserService = MockUserService.shared
+    var coordinator: AuthenticationCoordinator!
+    
+    func checkAvailableEmail(email: String) throws {
+        if (userService.getUserByEmail(email: email)) != nil {
+            throw ValidationError.init(atIndex: 1, type: .emailAlreadyInUse)
+        }
+    }
+    
+    func register(name: String, email: String, password: String) {
+        authenticationService.register(name: name, email: email, password: password)
+        // Start Profile coordiantor
+    }
+    
+    func switchToLoginPage() {
+        coordinator.changeToLogin()
+    }
+    
+}
