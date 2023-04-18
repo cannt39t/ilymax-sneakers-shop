88949585516298db6e3806632a79ecd34fd29ce2
Maksim Tarasov <77vwv@mail.ru>
2023-04-18 17:01:21 +0300

    some changes

---
+++ IlymaxShop/IlymaxShop/ShoeView/Presenters/ShoeViewPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  ShoeViewPresenter.swift
+//  IlymaxShop
+//
+//  Created by Максим Тарасов on 18.04.2023.
+//
+
+import UIKit
+
+class ShoeViewPresenter {
+    weak var view: ShoeViewController?
+    var coordinator: ShoeViewCoordinator!
+    
+    var product: Shoes? = nil
+
+}
---
+++ IlymaxShop/IlymaxShop/ShoeView/Services/ShoeViewService.swift
@@ -0,0 +1,8 @@
+//
+//  ShoeViewService.swift
+//  IlymaxShop
+//
+//  Created by Максим Тарасов on 18.04.2023.
+//
+
+import Foundation
