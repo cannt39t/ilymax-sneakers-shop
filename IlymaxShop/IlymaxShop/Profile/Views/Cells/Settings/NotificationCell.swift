ba1f465e98fc0dcbb97c3057a66bcf0b360a66b8
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-15 19:44:05 +0300

    Create NotificationCell.swift

---
+++ IlymaxShop/IlymaxShop/Profile/Views/Cells/Settings/NotificationCell.swift
@@ -0,0 +1,64 @@
+//
+//  NotificationCell.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 15.05.2023.
+//
+
+import UIKit
+
+
+class NotificationCell: UICollectionViewCell {
+    
+    public static let identifier = "NotificationCell"
+    private let nameLabel: UILabel = .init()
+    private let toggle: UISwitch = .init()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        
+        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+        setup()
+    }
+    
+    private func setupDesign() {
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+        
+        nameLabel.font = nameLabel.font.withSize(18)
+        nameLabel.textColor = .label
+    }
+    
+    private func setup() {
+        setupDesign()
+        
+        let mainStack = UIStackView(arrangedSubviews: [nameLabel, toggle])
+        mainStack.alignment = .leading
+        mainStack.spacing = 6
+        mainStack.axis = .horizontal
+        mainStack.distribution = .fill
+        
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(mainStack)
+        
+        NSLayoutConstraint.activate([
+            mainStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 9),
+            mainStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -9),
+            toggle.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            nameLabel.centerYAnchor.constraint(equalTo: toggle.centerYAnchor),
+        ])
+    }
+    
+    public func setName(_ text: String) {
+        nameLabel.text = text
+    }
+}
