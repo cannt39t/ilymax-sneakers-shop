d80e470408ea47135233bb749a9ba3e6a9fc8bb4
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-15 19:43:59 +0300

    Update ConversationsViewController.swift

--- IlymaxShop/IlymaxShop/Chat/Controllers/ConversationsViewController.swift
+++ IlymaxShop/IlymaxShop/Chat/Controllers/ConversationsViewController.swift
@@ -50,6 +50,7 @@ class ConversationsViewController: UIViewController {
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.separatorInsetReference = .fromCellEdges
     }
     
     private func startListeningForConversations() {
