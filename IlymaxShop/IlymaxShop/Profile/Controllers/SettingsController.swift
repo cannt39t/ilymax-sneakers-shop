bfc3beba35fa27ab4b4aed020fd5c05701b1b447
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-16 01:17:29 +0300

    Update SettingsController.swift

--- IlymaxShop/IlymaxShop/Profile/Controllers/SettingsController.swift
+++ IlymaxShop/IlymaxShop/Profile/Controllers/SettingsController.swift
@@ -40,6 +40,13 @@ class SettingsController: UIViewController {
         loader.dismiss(animated: true)
     }
     
+    func showSucces() {
+        loader.indicatorView = JGProgressHUDSuccessIndicatorView()
+        loader.textLabel.text = "Success"
+        loader.show(in: self.view, animated: true)
+        loader.dismiss(afterDelay: 1.0)
+    }
+    
 }
 
 extension SettingsController: UICollectionViewDataSource {
@@ -56,7 +63,7 @@ extension SettingsController: UICollectionViewDataSource {
             case 3:
                 return 3
             case 4:
-                return 1
+                return 2
             default:
                 return 0
         }
@@ -112,10 +119,40 @@ extension SettingsController: UICollectionViewDataSource {
                         cell.backgroundColor = .secondarySystemBackground
                         return cell
                 }
-//            case 3:
-//                return 3
-//            case 4:
-//                return 1
+            case 3:
+                switch indexPath.item {
+                    case 0:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HelpCenterCell.identifier, for: indexPath) as! HelpCenterCell
+                        cell.setName("FAQ")
+                        return cell
+                    case 1:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HelpCenterCell.identifier, for: indexPath) as! HelpCenterCell
+                        cell.setName("Contact Us")
+                        return cell
+                    case 2:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HelpCenterCell.identifier, for: indexPath) as! HelpCenterCell
+                        cell.setName("Privacy & Terms")
+                        return cell
+                    default:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+                        cell.backgroundColor = .secondarySystemBackground
+                        return cell
+                }
+            case 4:
+                switch indexPath.item {
+                    case 0:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ActionCell.identifier, for: indexPath) as! ActionCell
+                        cell.configure("Clear cache", .systemBlue)
+                        return cell
+                    case 1:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ActionCell.identifier, for: indexPath) as! ActionCell
+                        cell.configure("Sign out", .red)
+                        return cell
+                    default:
+                        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+                        cell.backgroundColor = .secondarySystemBackground
+                        return cell
+                }
             default:
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
                 cell.backgroundColor = .secondarySystemBackground
@@ -137,7 +174,7 @@ extension SettingsController: UICollectionViewDelegate {
     private func createSectionFor(index: Int, enviroment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
         switch index {
             case 4:
-                return setupSignOutSection()
+                return actionSection()
             default:
                 return setupFirstSection()
         }
@@ -166,19 +203,19 @@ extension SettingsController: UICollectionViewDelegate {
         return section
     }
     
-    private func setupSignOutSection() -> NSCollectionLayoutSection {
+    private func actionSection() -> NSCollectionLayoutSection {
         // item
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 12)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 12, bottom: 6, trailing: 12)
         
         //group
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(75))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(70))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         
         //section
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 24, trailing: 12)
         
         return section
     }
@@ -223,7 +260,7 @@ extension SettingsController: UICollectionViewDelegate {
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.backgroundColor = .systemBackground
+        collectionView.backgroundColor = .systemGroupedBackground
         
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(collectionView)
@@ -240,6 +277,22 @@ extension SettingsController: UICollectionViewDelegate {
         collectionView.register(PersonalnformationCell.self, forCellWithReuseIdentifier: PersonalnformationCell.identifier)
         collectionView.register(PasswordCell.self, forCellWithReuseIdentifier: PasswordCell.identifier)
         collectionView.register(NotificationCell.self, forCellWithReuseIdentifier: NotificationCell.identifier)
+        collectionView.register(HelpCenterCell.self, forCellWithReuseIdentifier: HelpCenterCell.identifier)
+        collectionView.register(ActionCell.self, forCellWithReuseIdentifier: ActionCell.identifier)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switch indexPath.section {
+            case 4 :
+                switch indexPath.item {
+                    case 0 :
+                        presenter.clearCache()
+                    default:
+                        print(indexPath)
+                }
+            default:
+                print(indexPath)
+        }
     }
     
 }
