6123fe2a74d6b7e055b70d547a3c2873da1746e9
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-18 00:28:54 +0300

    Create AddAddressPresenter.swift

---
+++ IlymaxShop/IlymaxShop/Profile/Presenters/AddAddressPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  AddAddressPresenter.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 17.05.2023.
+//
+
+import UIKit
+
+
+class AddAddressPresenter {
+    
+    weak var view: AddAddressViewController?
+    private let addressesService = AddAddressService()
+    
+    public func addAddress() {
+        
+    }
+}
+
