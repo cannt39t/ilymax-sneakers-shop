8a2409afb3b7b51137aaece8f4ee96fdfee5e186
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-17 22:20:17 +0300

    Update AddressesPresenter.swift

--- IlymaxShop/IlymaxShop/Profile/Presenters/AddressesPresenter.swift
+++ IlymaxShop/IlymaxShop/Profile/Presenters/AddressesPresenter.swift
@@ -13,11 +13,33 @@ class AddressesPresenter {
     weak var view: AddressesCollectionViewController?
     private let addressesService = AddressesService()
     public var currentUser: IlymaxUser
+    public var addresses: [IlymaxAddress] = []
     
     public var back: () -> () = {}
+    public var pushAddAddressController: () -> () = {}
     
     init(currentUser: IlymaxUser) {
         self.currentUser = currentUser
+    }
+    
+    
+    public func getAddresses() {
+        view?.showLoader()
+        addressesService.getAddreeses { [weak self] result in
+            switch result {
+                case .success(let addresses):
+                    self?.view?.hideLoader()
+                    self?.addresses = addresses
+                    if addresses.count == 0 {
+                        self?.view?.noAddressesView.isHidden = false
+                    } else {
+                        self?.view?.noAddressesView.isHidden = true
+                        self?.view?.collectionView.reloadData()
+                    }
+                case .failure(let error):
+                    print(error)
+            }
+        }
     }
     
     func backButtonTap() {
