cf8ff09a91ab6a9ce133ebe45f379e906eecf80f
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-18 17:55:08 +0300

    Update ShoeViewService.swift

--- IlymaxShop/IlymaxShop/ShoesView/Services/ShoeViewService.swift
+++ IlymaxShop/IlymaxShop/ShoesView/Services/ShoeViewService.swift
@@ -9,30 +9,7 @@ import UIKit
 import SDWebImage
 
 class ShoeViewService {
-    func getImage(for shoes: Shoes, completion: @escaping (UIImage?) -> Void) {
-        guard let imageUrl = shoes.imageUrl else {
-           completion(nil)
-           return
-       }
-       
-        StorageManager.shared.getImageUrlFromStorageUrl(imageUrl) { error, url in
-           if let error = error {
-               // Show error message to user
-               print("Error loading image: \(error.localizedDescription)")
-               completion(nil)
-               return
-           }
-           
-           guard let url = url else {
-               completion(nil)
-               return
-           }
-           
-           SDWebImageManager.shared.loadImage(with: url, options: [.progressiveLoad, .highPriority], progress: nil) { (image, _, _, _, _, _) in
-               completion(image)
-           }
-       }
-   }
+    
     
     func getReviewsByShoesId(_ shoesId: String, completion: @escaping (Result<[IlymaxReview], Error>) -> Void) {
         FirestoreManager.shared.getReviewsByShoesId(shoesId) { result in
