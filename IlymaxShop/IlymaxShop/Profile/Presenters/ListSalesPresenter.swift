36acc2f51e33219309cb8b79dbdc8234954aa4d3
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-18 17:54:51 +0300

    Update ListSalesPresenter.swift

--- IlymaxShop/IlymaxShop/Profile/Presenters/ListSalesPresenter.swift
+++ IlymaxShop/IlymaxShop/Profile/Presenters/ListSalesPresenter.swift
@@ -13,9 +13,22 @@ class ListSalesPresenter {
     weak var view: ListSalesController?
     private let listSalesService = ListSalesService()
     public var currentUser: IlymaxUser
+    public var listOfSales: [Shoes] = []
     
     init(currentUser: IlymaxUser) {
         self.currentUser = currentUser
+    }
+    
+    func getSaleList() {
+        listSalesService.getSaleList { [unowned self] result in
+            switch result {
+                case .success(let shoes):
+                    listOfSales = shoes
+                    view?.collectionView.reloadData()
+                case .failure(let error):
+                    print(error)
+            }
+        }
     }
     
 }
