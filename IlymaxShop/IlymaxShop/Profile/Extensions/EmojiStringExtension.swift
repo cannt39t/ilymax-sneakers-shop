cbaf1e6296a2a708ab340a2d4061745c926cda4a
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-18 13:03:02 +0300

    Create EmojiStringExtension.swift

---
+++ IlymaxShop/IlymaxShop/Profile/Extensions/EmojiStringExtension.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiStringExtension.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 18.05.2023.
+//
+
+import Foundation
+
+extension String {
+    
+    static func emojiFlag(for countryCode: String) -> String! {
+        func isLowercaseASCIIScalar(_ scalar: Unicode.Scalar) -> Bool {
+            return scalar.value >= 0x61 && scalar.value <= 0x7A
+        }
+        
+        func regionalIndicatorSymbol(for scalar: Unicode.Scalar) -> Unicode.Scalar {
+            precondition(isLowercaseASCIIScalar(scalar))
+            
+            return Unicode.Scalar(scalar.value + (0x1F1E6 - 0x61))!
+        }
+        
+        let lowercasedCode = countryCode.lowercased()
+        guard lowercasedCode.count == 2 else { return nil }
+        guard lowercasedCode.unicodeScalars.reduce(true, { accum, scalar in accum && isLowercaseASCIIScalar(scalar) }) else { return nil }
+        
+        let indicatorSymbols = lowercasedCode.unicodeScalars.map({ regionalIndicatorSymbol(for: $0) })
+        return String(indicatorSymbols.map({ Character($0) }))
+    }
+}
