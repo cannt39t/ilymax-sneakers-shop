df043778bddf14bb2a2869e8b0826eb90509cbad
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-28 13:23:23 +0300

    Update NotificationCell.swift

--- IlymaxShop/IlymaxShop/Profile/Views/Cells/Settings/NotificationCell.swift
+++ IlymaxShop/IlymaxShop/Profile/Views/Cells/Settings/NotificationCell.swift
@@ -12,7 +12,8 @@ class NotificationCell: UICollectionViewCell {
     
     public static let identifier = "NotificationCell"
     private let nameLabel: UILabel = .init()
-    private let toggle: UISwitch = .init()
+    public let toggle: UISwitch = .init()
+    var completion: (String, Bool) -> () = { _,_ in}
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -46,6 +47,7 @@ class NotificationCell: UICollectionViewCell {
     
     private func setup() {
         setupDesign()
+        toggle.addTarget(self, action: #selector(valueChange), for: .valueChanged)
         
         let mainStack = UIStackView(arrangedSubviews: [nameLabel, toggle])
         mainStack.alignment = .leading
@@ -63,6 +65,12 @@ class NotificationCell: UICollectionViewCell {
             toggle.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             nameLabel.centerYAnchor.constraint(equalTo: toggle.centerYAnchor),
         ])
+    }
+    
+    @objc func valueChange(mySwitch: UISwitch) {
+        let value = mySwitch.isOn
+        completion(nameLabel.text!, value)
+        print("switch value changed \(value)")
     }
     
     public func setName(_ text: String) {
