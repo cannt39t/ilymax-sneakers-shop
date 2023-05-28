06bc3eb750faba78675d29c1db85353cd85f1635
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-28 13:23:18 +0300

    Update SettingsPresenter.swift

--- IlymaxShop/IlymaxShop/Profile/Presenters/SettingsPresenter.swift
+++ IlymaxShop/IlymaxShop/Profile/Presenters/SettingsPresenter.swift
@@ -27,4 +27,12 @@ class SettingsPresenter {
     public func signOut() {
         settingsService.signOut()
     }
+    
+    public func changeState(for name: String, value: Bool) {
+        settingsService.changeNotification(nameNotifcation: name, value: value)
+    }
+    
+    public func getState(for name: String) -> Bool {
+        return settingsService.getCurrentState(for: name)
+    }
 }
