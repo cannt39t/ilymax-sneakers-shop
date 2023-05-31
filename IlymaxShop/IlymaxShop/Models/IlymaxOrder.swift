689262e3d01b78b09b3c99efd0b03a9ec31474d1
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-05-31 21:28:22 +0300

    Create IlymaxOrder.swift

---
+++ IlymaxShop/IlymaxShop/Models/IlymaxOrder.swift
@@ -0,0 +1,21 @@
+//
+//  IlymaxOrder.swift
+//  IlymaxShop
+//
+//  Created by Илья Казначеев on 31.05.2023.
+//
+
+import Foundation
+
+
+struct IlymaxOrder: FirestoreCollectionProtocol {
+    
+    static var collectionName: String = "orders"
+    
+    let id: String
+    let date: Date
+    let status: String
+    let customerId: String
+    
+    let items: [IlymaxCartItem]
+}
