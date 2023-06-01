c45cde3b388edf73fc3784612d3c5aed8788abda
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-06-01 03:04:08 +0300

    Update OrdersPresenter.swift

--- IlymaxShop/IlymaxShop/Profile/Presenters/OrdersPresenter.swift
+++ IlymaxShop/IlymaxShop/Profile/Presenters/OrdersPresenter.swift
@@ -12,8 +12,13 @@ class OrdersPresenter {
     weak var view: OrdersCollectionViewController?
     private let ordersService = OrdersService()
     public var currentUser: IlymaxUser
+    public var orders: [IlymaxOrder] = []
     
     init(currentUser: IlymaxUser) {
         self.currentUser = currentUser
+    }
+    
+    func fetchOrdersForCurrentUser() async throws {
+        orders = try await ordersService.getAllOrdersFor()
     }
 }
